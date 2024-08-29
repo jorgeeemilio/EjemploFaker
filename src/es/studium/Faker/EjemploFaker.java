de7d9a50d2b6588d43a87c5326551a9fa88bb90d
jorgeeemilio <jorge@grupostudium.com>
2024-08-29 08:17:04 +0200

    First commit

---
+++ src/es/studium/Faker/EjemploFaker.java
@@ -0,0 +1,37 @@
+package es.studium.Faker;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import com.github.javafaker.Faker;
+
+public class EjemploFaker
+{
+	public static void main(String[] args) {
+        // Crear una instancia de Faker
+        Faker faker = new Faker(new Locale("es"));
+
+        // Generar un nombre de persona
+        String name = faker.name().fullName();
+        System.out.println("Nombre: " + name);
+
+        // Generar un número de teléfono
+        String phoneNumber = faker.phoneNumber().phoneNumber();
+        System.out.println("Teléfono: " + phoneNumber);
+
+        // Generar una dirección
+        String address = faker.address().fullAddress();
+        System.out.println("Dirección: " + address);
+
+        // Generar un correo electrónico
+        String email = faker.internet().emailAddress();
+        System.out.println("Correo Electrónico: " + email);
+
+        // Generar una fecha de nacimiento
+        Date birthDate = faker.date().birthday();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        String formattedBirthDate = dateFormat.format(birthDate);
+        System.out.println("Fecha de Nacimiento: " + formattedBirthDate);
+    }
+}
